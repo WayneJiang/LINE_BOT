2a032a617600cac595755abca6eb73c3aec8adcc
WayneJiang <LibraFaith@gmail.com>
2025-02-22 14:44:12 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,6 @@ const client = new messagingApi.MessagingApiClient(clientConfig);
 const app: Application = express();
 
 app.get('/', async (_: Request, response: Response) => {
-    console.log('test')
     response.status(200).json({
         status: 'success',
         message: 'Connected successfully!',
@@ -30,14 +29,11 @@ app.get('/', async (_: Request, response: Response) => {
 });
 
 app.post('/callback', middleware(middlewareConfig), (request: Request, response: Response) => {
-    console.info('callback')
-
     Promise
         .all(request.body.events.map(handleEvent))
-        .then((result) => response.json(client.replyMessage({
-                replyToken: event.replyToken || '',
-                messages: [{ type: 'text', text: 'callback' }],
-            })))
+        .then((result) => //response.json(|)
+console.log(request.body);
+            )
         .catch((err) => {
             console.error(err);
             response.status(500).end();
@@ -50,11 +46,10 @@ function handleEvent(event: Event) {
         return Promise.resolve(null);
     }
 
-    console.info(event)
-
     client.getProfile(event.source?.userId || '')
         .then((result) => {
             // use reply API
+            console.log(result);
             return client.replyMessage({
                 replyToken: event.replyToken || '',
                 messages: [{ type: 'text', text: 'callback' }],
